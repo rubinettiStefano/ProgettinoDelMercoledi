e5093fd75e5347a33494107b2f90887eecd04545
rubinettiStefano <stefano.rubinetti@gmail.com>
2022-12-21 22:21:57 +0100

    Revert "Mondo in tedesco"
    This reverts commit d53622bc3abb3801f0d3c8ec0144a3c91fb0b3be.

--- main.java
+++ main.java
@@ -3,9 +3,6 @@ public class main
     public static void main(String[] args) 
     {
         //Ciao sono un main bellissimo    
-        System.out.println("Hello World");
-        System.out.println("Ciao Mondo");
-        System.out.println("Hola Mundo");
-        System.out.println("Bonjornen Monden");
+        System.out.println("Hello")
     }
 }
